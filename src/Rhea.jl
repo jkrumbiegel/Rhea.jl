4a0c7343433bf03933eb7f7487ad2adb8335df47
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-06-03 15:58:30 +0530

    Restructure main module
    Load code first, then export.

--- src/Rhea.jl
+++ src/Rhea.jl
@@ -1,26 +1,53 @@
 module Rhea
 
-export strength, is_required, required, strong, weak, medium
+## include files
+
 include("strength.jl")
-export variable, is_nil, nil_var, value, int_value, set_value, is
+
 include("variable.jl")
 
 include("approx.jl")
-export expression, add!, sub!, div!, mult!, substitute_out, coefficient
+
 include("expression.jl")
-export linear_expression, evaluate
+
 include("linear_expression.jl")
-export relation, eq, geq, leq, reverse_inequality
+
 include("relation.jl")
-export constraint, set_strength, is_required, is_inequality, is_satisfied
+
 include("constraint.jl")
+
+include("symbol.jl")
+
+include("simplex_solver.jl")
+
+## begin exports
+
+# strength
+export strength, is_required, required, strong, weak, medium
+
+# variable
+export variable, is_nil, nil_var, value, int_value, set_value, is
+
+# approx
+export expression, add!, sub!, div!, mult!, substitute_out, coefficient
+
+# linear_expression
+export linear_expression, evaluate
+
+# relation
+export relation, eq, geq, leq, reverse_inequality
+
+# constraint
+export constraint, set_strength, is_required, is_inequality, is_satisfied
+
+# symbol
 export symbol, is_nil, is_external, is_slack, is_pivotable, is_restricted,
     is_unrestricted, dummy, slack, errorsym, external
-include("symbol.jl")
+
+# simplex_solver
 export simplex_solver, row, add_constraint, add_constraints,
     update_external_variables, remove_constraint, set_constant,
     add_edit_var, add_edit_vars, suggest_value, remove_edit_var,
     has_edit_var, suggest
-include("simplex_solver.jl")
 
 end # module
